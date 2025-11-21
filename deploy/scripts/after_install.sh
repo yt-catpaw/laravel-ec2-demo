18ec0e8f362cec33645fc092fbd9499878758d5e
tajimayuki <softte2su77@gmail.com>
2025-11-21 18:04:41 +0900

    CodeDeploy用のappspec修正とAfterInstallスクリプトの権限周りを改善

--- deploy/scripts/after_install.sh
+++ deploy/scripts/after_install.sh
@@ -6,7 +6,7 @@ APP_DIR=/usr/share/nginx/html
 cd $APP_DIR
 
 # パーミッション調整（必要に応じて）
-chown -R ec2-user:ec2-user $APP_DIR
+# chown -R ec2-user:ec2-user $APP_DIR
 chmod -R ug+rwx storage bootstrap/cache
 
 # composer インストール（composer が /usr/bin/composer じゃなければパスを直す）
