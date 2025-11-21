611e3db0510ac95d8a63fdbf5cbd1f08fdeb432d
tajimayuki <softte2su77@gmail.com>
2025-11-21 19:07:28 +0900

    AfterInstallでLaravelの書き込み権限を正しく設定する処理を追加

--- deploy/scripts/after_install.sh
+++ deploy/scripts/after_install.sh
@@ -2,18 +2,27 @@
 set -e
 
 APP_DIR=/usr/share/nginx/html
+WEB_USER=nginx
+WEB_GROUP=nginx
 
-cd $APP_DIR
+cd "$APP_DIR"
 
-# パーミッション調整（必要に応じて）
-# chown -R ec2-user:ec2-user $APP_DIR
-chmod -R ug+rwx storage bootstrap/cache
+# 所有者変更（ここが今足りてないところ）
+chown -R "$WEB_USER":"$WEB_GROUP" \
+  storage \
+  bootstrap/cache
 
-# composer インストール（composer が /usr/bin/composer じゃなければパスを直す）
+# パーミッション調整
+find storage -type d -exec chmod 775 {} \;
+find storage -type f -exec chmod 664 {} \;
+
+find bootstrap/cache -type d -exec chmod 775 {} \;
+find bootstrap/cache -type f -exec chmod 664 {} \;
+
+# composer インストール
 /usr/bin/php /usr/bin/composer install --no-dev --optimize-autoloader
 
 # .env はEC2側に置いておく前提（Git管理しない）
 php artisan key:generate --force || true
-# php artisan migrate --force || true
 php artisan config:cache
 php artisan route:cache
