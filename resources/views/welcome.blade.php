2d6a317cafefd3bf00eed192f5f5e396f786df37
tajimayuki <softte2su77@gmail.com>
2025-11-21 16:41:01 +0900

    デプロイ確認用メッセージをwelcomeページに追加

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -53,6 +53,14 @@
             <main class="flex max-w-[335px] w-full flex-col-reverse lg:max-w-4xl lg:flex-row">
                 <div class="text-[13px] leading-[20px] flex-1 p-6 pb-12 lg:p-20 bg-white dark:bg-[#161615] dark:text-[#EDEDEC] shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d] rounded-bl-lg rounded-br-lg lg:rounded-tl-lg lg:rounded-br-none">
                     <h1 class="mb-1 font-medium">Let's get started</h1>
+                    
+                    <p class="mb-2 text-[#f53003] dark:text-[#FF4433]">
+                        🚀 Deployed via CodePipeline & CodeDeploy
+                    </p>
+                    <p class="mb-4 text-[#706f6c] dark:text-[#A1A09A]">
+                        デプロイ確認用メッセージです。この表示が出ていれば最新のデプロイが反映されています。
+                    </p>
+
                     <p class="mb-2 text-[#706f6c] dark:text-[#A1A09A]">Laravel has an incredibly rich ecosystem. <br>We suggest starting with the following.</p>
                     <ul class="flex flex-col mb-4 lg:mb-6">
                         <li class="flex items-center gap-4 py-2 relative before:border-l before:border-[#e3e3e0] dark:before:border-[#3E3E3A] before:top-1/2 before:bottom-0 before:left-[0.4rem] before:absolute">
